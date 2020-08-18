d569ebcde8c92a54bdfaff1f45b235b7c6826976
Matheus Gomes <mtgommes@gmail.com>
2020-08-18 02:57:12 -0300

    fix indentation and refactoring

--- app/repository/case.go
+++ app/repository/case.go
@@ -6,9 +6,9 @@ import (
 	"gopkg.in/guregu/null.v4"
 )
 
-const caseBaseQuery = `	SELECT c.id, c.owner, c.color, c.brand, c.resolved, c.moment, o.id, o.name
-						FROM "case" c
-						LEFT JOIN officer o on c.officer_id = o.id`
+const caseBaseQuery = `SELECT c.id, c.owner, c.color, c.brand, c.resolved, c.moment, o.id, o.name
+                       FROM "case" c
+                       LEFT JOIN officer o on c.officer_id = o.id`
 
 func (r *Repository) GetAllCases() ([]model.Case, error) {
 
@@ -70,8 +70,7 @@ func parseCaseRow(s scanner) (c model.Case, err error) {
 
 func (r *Repository) UpdateCase(c model.Case) (err error) {
 
-	query := `	UPDATE "case" SET owner = $1, color = $2, brand = $3, resolved = $4, moment = $5, officer_id = $6
-				WHERE id = $7`
+	query := `UPDATE "case" SET owner = $1, color = $2, brand = $3, resolved = $4, moment = $5, officer_id = $6 WHERE id = $7`
 
 	_, err = r.db.Exec(query, c.Owner, c.Color, c.Brand, c.Resolved, c.Moment, c.Officer.Id, c.Id)
 
@@ -80,9 +79,7 @@ func (r *Repository) UpdateCase(c model.Case) (err error) {
 
 func (r *Repository) InsertCase(c model.Case, officerId null.Int) (id null.Int, err error) {
 
-	query := `	INSERT INTO "case"(owner, color, brand, resolved, moment, officer_id) 
-				VALUES ($1, $2, $3, $4, $5, $6) 
-				RETURNING id`
+    query := `INSERT INTO "case"(owner, color, brand, resolved, moment, officer_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`
 
 	err = r.db.QueryRow(query, c.Owner, c.Color, c.Brand, c.Resolved, c.Moment, officerId).Scan(&id)
 
@@ -91,8 +88,7 @@ func (r *Repository) InsertCase(c model.Case, officerId null.Int) (id null.Int, 
 
 func (r *Repository) ResolveCase(id int) (officerID null.Int, err error) {
 
-	query := `	UPDATE "case" c SET resolved = true WHERE id = $1 
-				RETURNING c.officer_id`
+	query := `UPDATE "case" c SET resolved = true WHERE id = $1 RETURNING c.officer_id`
 
 	err = r.db.QueryRow(query, id).Scan(&officerID)
 
@@ -101,8 +97,7 @@ func (r *Repository) ResolveCase(id int) (officerID null.Int, err error) {
 
 func (r *Repository) AssignOpenCase(officerID null.Int) (err error) {
 
-	query := `	UPDATE "case" SET officer_id = $1 
-				WHERE id = (SELECT id FROM "case" WHERE resolved = false LIMIT 1)`
+	query := `UPDATE "case" SET officer_id = $1 WHERE id = (SELECT id FROM "case" WHERE resolved = false LIMIT 1)`
 
 	_, err = r.db.Exec(query, officerID)
 
--- app/repository/officer.go
+++ app/repository/officer.go
@@ -1,14 +1,15 @@
 package repository
 
 import (
+	"fmt"
 	"github.com/matgomes/stolen-bike-challenge/app/model"
 )
 
 func (r *Repository) FindAvailableOfficer() (result model.Officer, err error) {
 
-	query := `	SELECT id, name FROM officer WHERE id 
-				NOT IN (SELECT officer_id FROM "case" WHERE resolved = false AND officer_id IS NOT NULL) 
-				LIMIT 1`
+	queryBusyOfficer := `SELECT officer_id FROM "case" WHERE resolved = false AND officer_id IS NOT NULL`
+
+	query := fmt.Sprintf(`SELECT id, name FROM officer WHERE id NOT IN (%s) LIMIT 1`, queryBusyOfficer)
 
 	err = r.db.QueryRow(query).Scan(&result.Id, &result.Name)
 
@@ -17,8 +18,7 @@ func (r *Repository) FindAvailableOfficer() (result model.Officer, err error) {
 
 func (r *Repository) InsertOfficer(o model.Officer) (id int, err error) {
 
-	query := `	INSERT INTO officer(name) VALUES ($1) 
-				RETURNING id`
+	query := `INSERT INTO officer(name) VALUES ($1) RETURNING id`
 
 	err = r.db.QueryRow(query, o.Name).Scan(&id)
 
@@ -27,8 +27,7 @@ func (r *Repository) InsertOfficer(o model.Officer) (id int, err error) {
 
 func (r *Repository) RemoveOfficer(id int) (deleted bool, err error) {
 
-	query := `	WITH deleted AS (DELETE FROM officer WHERE id = $1 RETURNING *) 
-				SELECT count(*) FROM deleted`
+	query := `WITH deleted AS (DELETE FROM officer WHERE id = $1 RETURNING *) SELECT count(*) FROM deleted`
 
 	var count int
 	err = r.db.QueryRow(query, id).Scan(&count)
